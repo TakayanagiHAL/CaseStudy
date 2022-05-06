a7179246052b3e0e961561b77832f92e13d73592
harada <doesn.t.mind._l_.627@icloud.com>
2022-05-06 22:13:28 +0900

    泡のアニメーションを追加

--- Assets/harada/Scripts/EffectManager.cs
+++ Assets/harada/Scripts/EffectManager.cs
@@ -6,16 +6,21 @@ using UnityEngine.InputSystem;
 public class EffectManager : MonoBehaviour
 {
     [SerializeField] GameObject EffectPrefab;
-    [SerializeField] Vector3 CreatePos;
+    [SerializeField] Vector3 CreatePosV;
+    [SerializeField] Transform CreatePosT;
     [SerializeField] bool Loop;
+    [SerializeField] bool Chase;
+
 
     GameObject EffectObject;
-    
+    Animator effectAnimator;
+    AnimatorClipInfo[] effectAnimatorClipInfo;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -26,7 +31,7 @@ public class EffectManager : MonoBehaviour
         // エフェクトお試し生成
         if (Keyboard.current.cKey.wasReleasedThisFrame)
         {
-            CreateEffect();
+            CreateEffect(90.0f);
         }
 
         // エフェクトお試し消去
@@ -36,15 +41,31 @@ public class EffectManager : MonoBehaviour
         }
 #endif
 
+        // Chaseがtrueならtransformの座標に追従する
+        if(Chase && EffectObject != null)
+        {
+            EffectObject.transform.position = CreatePosT.position;
+        }
+
 
     }
 
     // エフェクトの生成
-    void CreateEffect()
+    // effectRotate...回転させたい角度の値（Z回転）
+    public void CreateEffect(float effectRotate)
     {
         // インスタンス生成
-        EffectObject = Instantiate(EffectPrefab, CreatePos, Quaternion.identity);
+        // Transformが設定されていなければ指定した座標で生成
+        if (CreatePosT != null)
+        {
+            EffectObject = Instantiate(EffectPrefab, CreatePosT.transform.position, Quaternion.Euler(0, 0, effectRotate));
+        }
+        else
+        {
+            EffectObject = Instantiate(EffectPrefab, CreatePosV, Quaternion.Euler(0, 0, effectRotate));
+        }
 
+        
         // ループ設定がオフならばアニメーション終了でインスタンスを消去
         if(!Loop)
         {
@@ -57,8 +78,14 @@ public class EffectManager : MonoBehaviour
     // エフェクトの消去
     void DestroyEffect()
     {
+        // アニメーションクリップの取得
+        effectAnimator = EffectObject.GetComponent<Animator>();
+        effectAnimatorClipInfo = effectAnimator.GetCurrentAnimatorClipInfo(0);
+
+        //Debug.Log(effectAnimatorClipInfo[0].clip.length);
+
         // インスタンス消去
-        GameObject.Destroy(EffectObject, 1.0f);
+        GameObject.Destroy(EffectObject, effectAnimatorClipInfo[0].clip.length);
     }
 
 }
