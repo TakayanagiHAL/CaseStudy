f7b95bafcb174499f8d8728ae750afebd9f177d5
JCRC1 <j.campo.RC@gmail.com>
2022-05-24 16:31:07 +0900

    Added images to the UI

---
+++ Assets/Campo/Scripts/StringToImage.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class StringToImage : MonoBehaviour
+{
+    public enum SUUJI
+    {
+        zero,
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine
+    }
+
+    public Sprite[] sprites;
+    public Image[] images;
+    public float timerValue;
+
+    private string timerAsString;
+    private string[] splitString;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        splitString = new string[6];
+        // Get the float timer as string
+        timerAsString = timerValue.ToString("000.00");
+
+        // Separate string into each individual char
+        for (int i = 0; i < timerAsString.Length; i++)
+        {
+            splitString[i] = timerAsString[i].ToString();
+        }
+
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (i == 3)
+                continue;
+            // Parse the numbers in the string
+            int number;
+            number = int.Parse(splitString[i]);
+
+            switch ((SUUJI)number)
+            {
+                case SUUJI.zero:
+                    images[i].sprite = sprites[0];
+                    break;
+                case SUUJI.one:
+                    images[i].sprite = sprites[1];
+                    break;
+                case SUUJI.two:
+                    images[i].sprite = sprites[2];
+                    break;
+                case SUUJI.three:
+                    images[i].sprite = sprites[3];
+                    break;
+                case SUUJI.four:
+                    images[i].sprite = sprites[4];
+                    break;
+                case SUUJI.five:
+                    images[i].sprite = sprites[5];
+                    break;
+                case SUUJI.six:
+                    images[i].sprite = sprites[6];
+                    break;
+                case SUUJI.seven:
+                    images[i].sprite = sprites[7];
+                    break;
+                case SUUJI.eight:
+                    images[i].sprite = sprites[8];
+                    break;
+                case SUUJI.nine:
+                    images[i].sprite = sprites[9];
+                    break;
+            }
+        }        
+    }
+}
--- Assets/GameManagement/GameManagement.cs
+++ Assets/GameManagement/GameManagement.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class GameManagement : MonoBehaviour
 {
-    [SerializeField] Canvas gameCanvas;
-    [SerializeField] Canvas clearCanvas;
-    [SerializeField] Canvas overCanvas;
-    [SerializeField] Canvas poseCanvas;
-    [SerializeField] Timer timer;
-    [SerializeField] lifeUI life;
+    public Canvas[] canvas;
+    public bool[] bools;
+
+    public Timer timer;
+    public lifeUI life;
+
     // Start is called before the first frame update
     void Start()
     {
-        clearCanvas.enabled = false;
-        overCanvas.enabled = false;
-        poseCanvas.enabled = false;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < bools.Length; i++)
+        {
+            foreach (Canvas c in canvas)
+            {
+                if (c != canvas[i])
+                {
+                    canvas[i].enabled = bools[i];
+                }
+            }
+        }
     }
 }
