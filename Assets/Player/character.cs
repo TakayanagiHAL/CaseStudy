a240bc88d5cf8a4dbcae70d103dea20232a0d608
harada <doesn.t.mind._l_.627@icloud.com>
2022-07-12 17:27:17 +0900

    泡のパーティクルと運搬物の浮遊実装

--- Assets/Player/character.cs
+++ Assets/Player/character.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class character : MonoBehaviour
 {
-
+    [SerializeField] float Width;
+    [SerializeField] int Speed;
+    float count = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,9 @@ public class character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        count += 0.01f;
+
+        transform.position += new Vector3(0.0f, Mathf.Sin(count) * Width, 0.0f);
     }
 
  
